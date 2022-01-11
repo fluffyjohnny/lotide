19d91b15403644ddcee3c3466bc6c221b0a90ec8
John Chia <john.chia0106@gmail.com>
2022-01-10 16:11:44 -0800

    added the assertEqual file to the library

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log('⛔ Assertion Failed: ' + actual + ' !== ' + expected);
+  } else if (actual === expected) {
+    console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('haha', 'haha');
+assertEqual(3, 4);
