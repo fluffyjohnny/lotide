35cdd656cf350a4ae47aa3dca5dbf334daef4df0
John Chia <john.chia0106@gmail.com>
2022-01-11 18:10:43 -0800

    added the without.js file

---
+++ without.js
@@ -0,0 +1,20 @@
+const without = (wantedArr, unwantedArr) => {
+  let newArr = [];
+  for (let i = 0; i < wantedArr.length; i++) {
+    let seen = false;
+    for (let j = 0; j < unwantedArr.length; j++) {
+      if (wantedArr[i] === unwantedArr[j]) {
+        seen = true;
+        break;
+      }
+    }
+    if (!seen) {
+      newArr.push(wantedArr[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(without([1, 2, 3, 4, 1,], [1, 2]));
+
+
