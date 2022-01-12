723aeb4c3e22de2351cf40e48d2977c785d9145b
John Chia <john.chia0106@gmail.com>
2022-01-11 21:35:39 -0800

    updated eqArrays.js to be more flexible

--- eqArrays.js
+++ eqArrays.js
@@ -7,12 +7,11 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-  let str1 = arr1.join(', ');
-  let str2 = arr2.join(', ');
-  if (str1 === str2) {
+  for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
     return true;
-  } else {
-    return false;
   }
 };
 
