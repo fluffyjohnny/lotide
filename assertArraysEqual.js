0937694a8088453aa1c97d48e01a059742bd4082
John Chia <john.chia0106@gmail.com>
2022-01-11 22:10:52 -0800

    improved the functionality of asserArrayEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,16 @@
-const assertArrayEqual = function(actual, expected) {
-  let str1 = actual.join(', ');
-  let str2 = expected.join(', ');
-  if (str1 !== str2) {
-    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
-  } else if (str1 === str2) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+    return true;
   }
+};
+
+const assertArrayEqual = (actual, expected) => {
+  eqArrays(actual, expected)
+    ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // test
