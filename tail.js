0ceacc240aa7fb666b7a6690102902034c2f7bc8
John Chia <john.chia0106@gmail.com>
2022-01-17 10:31:34 -0800

    modulized tail.js and created tailTest.js

--- tail.js
+++ tail.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 let tail = (arr) => {
   let sliceArr = arr.slice(1);
   return sliceArr;
 };
 
+module.exports = tail;
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
