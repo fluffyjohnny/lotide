c67e8ef369b733f2877e637bb8df517793b2cf13
John Chia <john.chia0106@gmail.com>
2022-01-17 13:14:08 -0800

    modulized assertArraysEqual and created test for it

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,9 @@
-const eqArrays = (arr1, arr2) => {
-  for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const assertArrayEqual = (actual, expected) => {
+const assertArraysEqual = (actual, expected) => {
   eqArrays(actual, expected)
     ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// test
-assertArrayEqual([1, 2, 3], [3, 4 ,5]);
-assertArrayEqual([1, 2, 3], [1, 2 ,3]);
+module.exports = assertArraysEqual;
---
+++ test/assertArrayEqual.test.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+// test
+assertArraysEqual([1, 2, 3], [3, 4 ,5]);
+assertArraysEqual([1, 2, 3], [1, 2 ,3]);
